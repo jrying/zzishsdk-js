241c8c446db24ccbf526c49c987b9411d215485f
Samir Seetal <sseetal@gmail.com>
2015-01-13 12:36:41 +0000

    Get token from callback

--- zzish.js
+++ zzish.js
@@ -640,7 +640,6 @@
             //create a token first
             sendData(token_request, function (err, data) {
                 callCallBack(err, data, function(err,data) {                
-                    console.log('user', data);
                     data.token = token;
                     callback(err,data);             
                 });
